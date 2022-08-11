aa696f2d6eff5f9e49c559225a0242b252a2965a
Dylan Hays <twentygx@outlook.com>
2022-08-10 20:58:36 -0500

    Commented code, removed unnecessary debug statements

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ let results;
 const baseURL = "https://datausa.io/api/data?";
 
 document.addEventListener("DOMContentLoaded", () => {
+    //Get necessary elements from DOM
     occ = document.querySelector('#occupations');
     loc = document.querySelector('#locations');
     dPoints = document.querySelector('#dataPoints');
@@ -13,43 +14,51 @@ document.addEventListener("DOMContentLoaded", () => {
     submit = document.querySelector('#submit');
     submit.addEventListener('click', (e) => getData(e));
 
+    //Populate the dropdowns
     populateSelect(occ, occupations);
     populateSelect(loc, states);
     populateSelect(dPoints, dataPoints);
 })
 
+//This is called when submitting the form
 async function getData(e) {
     e.preventDefault();
+    //First we grab the location data
     let url = baseURL;
     url += states.apiCall(loc.value);
     url += `&${dataPoints.apiCall(dPoints.value)}&year=latest`;
     let locResult = await fetchData(url)
     .then(arr => arr.data[0][dPoints.value]);
-    appendNewChild(results, 'p', {"text": `${dPoints.value} in ${loc.value}: ${dataPoints[dPoints.value].format(locResult)}`});
+    //Append location data to DOM
+    appendNewChild(results, 'p', {text: `${dPoints.value} in ${loc.value}: ${dataPoints[dPoints.value].format(locResult)}`});
+    
+    //Now we do the same for the occupation
     url += "&" + occupations.apiCall(occ.value)[0];
     let occResult = await fetchData(url)
     .then(arr => arr.data[0][dPoints.value]);
-    appendNewChild(results, 'p', {"text": `${dPoints.value} of ${occ.value} in ${loc.value}: ${dataPoints[dPoints.value].format(occResult)}`});
+    appendNewChild(results, 'p', {text: `${dPoints.value} of ${occ.value} in ${loc.value}: ${dataPoints[dPoints.value].format(occResult)}`});
 }
-console.log(dataPoints["Average Wage"].format("1000000.15"));
+
+//Just used to simplify fetch requests
 async function fetchData(url) {
     return await fetch(url)
     .then(d => d.json());
 }
 
+//Function to populate the dropdowns
 function populateSelect(select, keys) {
     for (const options of Object.keys(keys)) {
         if (options != "apiCall")
-            appendNewChild(select, 'option', {'value': options, 'text': options});
+            appendNewChild(select, 'option', {value: options, text: options});
     }
 }
 
+//Helper functions to make adding new elements to the DOM a bit easier
 function appendNewChild(parent, tag, attr) {
     const ne = newElement(tag, attr);
     parent.appendChild(ne);
     return ne;
-}
-  
+
   function newElement(tag, attr) {
     let ne = document.createElement(tag);
     if (attr != null)
@@ -63,3 +72,4 @@ function appendNewChild(parent, tag, attr) {
         });
     return ne;
   }
+}
