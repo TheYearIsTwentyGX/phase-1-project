059477650ba472521249db37c4cdfb6aa836e677
Dylan Hays <twentygx@outlook.com>
2022-08-27 17:10:55 -0500

    Reworked Average Wage to send to a table

--- src/dataFiles/refs.js
+++ src/dataFiles/refs.js
@@ -8,7 +8,26 @@ let stateSelects;
 let subfilters = [ {value: undefined} ];
 let submit;
 
+const ignoreProps = ["apiCall", "generalData", "scopes"];
+const ignoreStates = ["District of Columbia", "Puerto Rico"];
+
 const UrlStyles = {
     Basic: "https://datausa.io/api/data?",
     Uranium: "https://api-ts-uranium.datausa.io/tesseract/data?"
 }
+
+function reloadArgs() {
+    if (dataSets[currentIndex].value === "Health")
+        dataPoints["Health"].arg = (function() { return `measure=${health[filters[currentIndex].value][subfilters[currentIndex].value].measure}`; })();
+}
+
+/* PROPERTIES
+loc
+subject
+value
+formattedValue
+text
+
+rank
+color
+*/
